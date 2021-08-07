ab7a28a442e7483b747fba7b46bb8b85d03f3d3d
kajalkarbhal@gmail.com <kajalkarbhal@gmail.com>
2021-08-07 12:23:33 +0530

    added Sub code

--- Sum.java
+++ Sum.java
@@ -1,6 +1,10 @@
 public class Sum{
 public static void main(String a[]){
-int num1=5,num2=15,sum;
+int num1=5,num2=15,sum,sub;
 sum=num1+num2;
-System.out.println("Sum of these numbes:"+sum)
+System.out.println("Sum of these numbes:"+sum);
+sub=num2-num1;
+System.out.println("Substraction of these numbes:"+sub);
+
+
 }
